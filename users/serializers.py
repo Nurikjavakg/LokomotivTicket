198f9ccd2824597e46bad19133892a99a538d52e
baktulan <bak2landeveloper@gmail.com>
2025-11-03 10:51:39 +0600

    Added new models User

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'phone', 'role', 'first_name', 'last_name')
+        read_only_fields = ('id', 'role')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True)    
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'phone', 'password', 'password_confirm', 'first_name', 'last_name')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({"password": "Пароли не совпадают"})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user   
